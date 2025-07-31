4ad92d5f356a4f9e9acab31c333ade9e0f5033fe
vaishnavidhavale <163302851+vaishnavidhavale@users.noreply.github.com>
2025-08-01 02:32:18 +0530

    My new vs code

---
+++ src/main/java/com/github/github_new_project/GithubNewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.github_new_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubNewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubNewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/github_new_project/GithubNewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.github_new_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubNewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
